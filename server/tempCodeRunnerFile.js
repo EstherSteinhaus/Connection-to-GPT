93a9fbf65b5d16b91d0f131f25c6b06125b9eac5
EstherSteinhaus <esther44736@gmail.com>
2024-04-02 11:58:22 +0300

    pushing project

--- server/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-const response=await openai.chat.completions.create({
-//         messages:[{content:`${question} in ${url}`,role:"user"}],
-//         model:"gpt-3.5-turbo"
-// })
-//     console.log(response.choices[0]);
